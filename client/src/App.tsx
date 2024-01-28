131a22b65e34d6d6de0a38122dbc0e56f911da65
Marjeck Palacpac <marjeck@advabilities.com>
2024-01-28 14:17:32 +0800

    Creating sample product in the react app. Use react hooks (useState, useEffect). Remove styling inside the index.css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react"
+
 function App() {
+  const [products, setProducts] = useState([
+    { name: 'product1', price: 100.00 },
+    { name: 'product2', price: 201.00 },
+  ]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, [])
+
+  function addProduct()
+  {
+    setProducts(prevState => [...products, 
+      {name: 'product' + (prevState.length + 1), price: (prevState.length * 100) + 100}])
+  }
+
   return (
     <div>
       <h1>Re-Store</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>{item.name} - {item.price}</li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>Add product</button>
     </div>
   )
 }
