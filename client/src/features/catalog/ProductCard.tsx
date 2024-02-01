95a8b3259e3169dc5380bebf836ea3e72be801f3
Marjeck Palacpac <marjeck@advabilities.com>
2024-02-01 21:51:44 +0800

    Adding cards for individual products

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,17 +1,31 @@
-import { ListItem, ListItemAvatar, Avatar, ListItemText } from "@mui/material";
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
 import { Product } from "../../app/models/product";
 
 interface Prop {
     product: Product;
 }
 
-export default function ProductCard({product}: Prop) {
+export default function ProductCard({ product }: Prop) {
     return (
-        <ListItem key={product.id}>
-            <ListItemAvatar>
-                <Avatar src={product.pictureUrl}></Avatar>
-            </ListItemAvatar>
-            <ListItemText>{product.name} - {product.price}</ListItemText>
-        </ListItem>
+        <Card>
+            <CardMedia
+                sx={{ height: 140 }}
+                image="http://picsum.photos/200"
+                title="green iguana"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+        </Card>
     )
 }
