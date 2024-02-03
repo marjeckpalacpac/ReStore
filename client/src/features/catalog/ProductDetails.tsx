79e034cf60633b6aac0a8bbd4da298bf8259d50d
Marjeck Palacpac <marjeck@advabilities.com>
2024-02-03 17:47:17 +0800

    Adding the product detail page content pt.2

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -14,15 +14,49 @@ export default function ProductDetails() {
             .then(response => setProduct(response.data))
             .catch(error => console.log(error))
             .finally(() => setLoading(false))
-    })
+    },[id])
 
     if (loading) return <h3>Loading...</h3>
 
     if (!product) return <h3>Product not found</h3>
 
     return (
-        <Typography variant="h2">
-            {product.name}
-        </Typography>
+        
+        <Grid container spacing={6}>
+            <Grid item xs={6}>
+                <img src={product.pictureUrl} alt={product.name} style={{width: '100%'}}/>
+            </Grid>
+            <Grid item xs={6}>
+                <Typography variant="h3">{product.name}</Typography>
+                <Divider sx={{mb: 2}} />
+                <Typography variant="h3" color="secondary">{(product.price / 100).toFixed(2)}</Typography>
+                <TableContainer>
+                    <Table>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell>{product.name}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Description</TableCell>
+                                <TableCell>{product.name}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Type</TableCell>
+                                <TableCell>{product.type}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Brand</TableCell>
+                                <TableCell>{product.brand}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>Quantity in stock</TableCell>
+                                <TableCell>{product.quantityInStock}</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Grid>
+        </Grid>
     )
 }
