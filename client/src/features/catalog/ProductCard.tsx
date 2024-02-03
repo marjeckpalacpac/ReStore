e7b691cd98d0b6da42004ffb375b2fe5ce01721b
Marjeck Palacpac <marjeck@advabilities.com>
2024-02-03 15:24:52 +0800

    Adding the product detail page conent

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,5 +1,6 @@
 import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
 import { Product } from "../../app/models/product";
+import { Link } from "react-router-dom";
 
 interface Prop {
     product: Product;
@@ -34,7 +35,7 @@ export default function ProductCard({ product }: Prop) {
             </CardContent>
             <CardActions>
                 <Button size="small">Add to cart</Button>
-                <Button size="small">View</Button>
+                <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
             </CardActions>
         </Card>
     )
