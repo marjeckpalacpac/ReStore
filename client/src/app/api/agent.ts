cc811bac9e7de5938ff35304c67c47302e53745e
Marjeck Palacpac <marjeck.palacpac@ogglobal.net>
2024-02-08 09:09:27 +0800

    Setting up linting

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -46,8 +46,8 @@ axios.interceptors.response.use(async response => {
 
 const request = {
     get: (url: string) => axios.get(url).then(responseBody),
-    post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
-    put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
+    post: (url: string, body: object) => axios.post(url, body).then(responseBody),
+    put: (url: string, body: object) => axios.put(url, body).then(responseBody),
     delete: (url: string) => axios.delete(url).then(responseBody),
 }
 
