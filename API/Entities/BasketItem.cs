0e70db0c7d4b806e8130067fe0ff0743551ec08a
Marjeck Palacpac <marjeck@advabilities.com>
2024-02-10 21:44:00 +0800

    EF Relationships (Basket & BasketItems)

--- API/Entities/BasketItem.cs
+++ API/Entities/BasketItem.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.Entities
 {
+    [Table("BasketItems")]
     public class BasketItem
     {
         public int Id { get; set; }
@@ -12,5 +14,8 @@ namespace API.Entities
 
         public int ProductId { get; set; }
         public Product Product { get; set; }
+
+        public int BasketId { get; set; }
+        public Basket Basket { get; set; }
     }
 }
