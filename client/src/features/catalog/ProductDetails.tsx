efb4e2f4be36ad4b4943f0004f2131658e50135b
Marjeck Palacpac <marjeck@advabilities.com>
2024-02-13 21:41:23 +0800

    Updating the product detail component to add item to cart part 1

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,37 +1,44 @@
-import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
+import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, TextField, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
 import agent from "../../app/api/agent";
 import NotFound from "../../app/errors/NotFound";
 import LoadingCompoent from "../../app/layout/LoadingComponent";
+import { useStoreContext } from "../../app/context/StoreContext";
+import { LoadingButton } from "@mui/lab";
 
 export default function ProductDetails() {
-    const {id} = useParams<{id: string}>();
+    const { basket } = useStoreContext();
+    const { id } = useParams<{ id: string }>();
     const [product, setProduct] = useState<Product | null>(null);
     const [loading, setLoading] = useState(true);
+    const [quantity, setQuantity] = useState(0);
+    const [submitting, setSubmitting] = useState(false)
+    const item = basket?.items.find(i => i.productId === product?.id);
 
     useEffect(() => {
+        if (item) setQuantity(item.quantity);
         id && agent.Catalog.details(parseInt(id))
-        // axios.get(`http://localhost:5001/api/products/${id}`)
+            // axios.get(`http://localhost:5001/api/products/${id}`)
             .then(response => setProduct(response))
             .catch(error => console.log(error))
             .finally(() => setLoading(false))
-    },[id])
+    }, [id, item])
 
-    if (loading) return <LoadingCompoent message='Loading product...'/>
+    if (loading) return <LoadingCompoent message='Loading product...' />
 
     if (!product) return <NotFound />
 
     return (
-        
+
         <Grid container spacing={6}>
             <Grid item xs={6}>
-                <img src={product.pictureUrl} alt={product.name} style={{width: '100%'}}/>
+                <img src={product.pictureUrl} alt={product.name} style={{ width: '100%' }} />
             </Grid>
             <Grid item xs={6}>
                 <Typography variant="h3">{product.name}</Typography>
-                <Divider sx={{mb: 2}} />
+                <Divider sx={{ mb: 2 }} />
                 <Typography variant="h3" color="secondary">{(product.price / 100).toFixed(2)}</Typography>
                 <TableContainer>
                     <Table>
@@ -59,6 +66,29 @@ export default function ProductDetails() {
                         </TableBody>
                     </Table>
                 </TableContainer>
+                <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                        <TextField
+                            variant='outlined'
+                            type='number'
+                            label='Quantity in Cart'
+                            fullWidth
+                            value={quantity}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <LoadingButton
+                            sx={{ height: '55px' }}
+                            color='primary'
+                            size='large'
+                            variant='contained'
+                            fullWidth
+                        >
+                            {item ? 'Update Quantity' : 'Add to Cart'}
+
+                        </LoadingButton>
+                    </Grid>
+                </Grid>
             </Grid>
         </Grid>
     )
