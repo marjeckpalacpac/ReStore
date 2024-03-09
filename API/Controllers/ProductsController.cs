4b37146539e5b9d3314961ba9bd339cc77ed30f0
Marjeck Palacpac <marjeck@advabilities.com>
2024-03-09 23:23:22 +0800

    Testing the pagination in swagger. Make camel case the pagination from response using PropertyNamingPolicy

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -18,7 +18,7 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<PagedList<Product>>> GetProduct(ProductParams productParams)
+        public async Task<ActionResult<PagedList<Product>>> GetProduct([FromQuery]ProductParams productParams)
         {
             var query = _context.Products
                 .Sort(productParams.OrderBy)
@@ -29,7 +29,7 @@ namespace API.Controllers
 
             var products = await PagedList<Product>.ToPagedList(query, productParams.PageNumber, productParams.PageSize);
 
-            Response.Headers.Add("Pagination", JsonSerializer.Serialize(products.MetaData));
+            Response.AddPaginationHeader(products.MetaData);
 
             return products;
         }
